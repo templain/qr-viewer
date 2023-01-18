0d6eb38b8d1d873d30b75e7b55e6cbbbc6cd8531
grandmacks <grandmacks.gmail.com>
2023-01-18 22:53:58 +0000

    change version

--- qrcode-viewer/src/main/java/com/qrcode/viewer/App.java
+++ qrcode-viewer/src/main/java/com/qrcode/viewer/App.java
@@ -2,18 +2,11 @@ package com.qrcode.viewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class App extends SpringBootServletInitializer {
+public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(App.class);
     }
 }
